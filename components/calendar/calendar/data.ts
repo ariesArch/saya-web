2bbb0d6880e509139f0414e6642ba905fd25802b
Thet Aung <thetaung.dev@gmail.com>
2022-06-18 00:59:32 +0630

    fixing calendar.interfaces.ts file so that it doesn't break the production code

--- components/calendar/calendar/data.ts
+++ components/calendar/calendar/data.ts
@@ -1,3 +1,13 @@
 import { WeekDayItem } from "@/interfaces/calendar.interfaces";
 
+type DayStatus = "Good enough" | "Slow go go" | "Hard learning" | "Take a nap";
+
+export const mapDayStatus: DayStatus[] = [
+    "Take a nap",
+    "Slow go go",
+    "Slow go go",
+    "Good enough",
+    "Hard learning",
+];
+
 export const data: WeekDayItem[] = [];
